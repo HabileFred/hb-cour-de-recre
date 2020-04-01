0e37e87c730088ec3684fe4dbaf3b560b575ab4d
Frédéric Bonjour <fred@habilebill.fr>
2020-04-01 11:42:09 +0200

    Upload files to test server.

--- build_prod.sh
+++ build_prod.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 PUBLIC_PATH='/cour-de-recre/' BUILD_FOLDER_PATH='build/cour-de-recre' npm run build
+scp build/cour-de-recre/* glimpsef@ssh.cluster011.hosting.ovh.net:www/cour-de-recre/
+
