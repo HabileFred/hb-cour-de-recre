ea66210749a0f330d0f7e80c5aa17e4b197a2442
Frédéric Bonjour <fred@habilebill.fr>
2020-04-03 09:39:53 +0200

    Lightbulbs are blinking when they are turned on.

---
+++ app/containers/BiduleOTron/parts/Indicators/Indicators.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+import imgOn from './img/ampoule_on.png';
+import imgOff from './img/ampoule_off.png';
+
+const IndicatorsContainer = styled.div`
+  position: absolute;
+  top: 90px;
+  left: 1120px;
+  width: 60px;
+  height: 260px;
+`;
+
+const IndicatorOff = styled.div`
+  position: absolute;
+  left: ${props => props.index}px;
+  top: ${props => props.index * 31}px;
+  width: 48px;
+  height: 42px;
+  background: center center no-repeat url('${imgOff}');
+`;
+
+const onAnimation = keyframes`
+  from {
+    opacity: 0;
+  }
+  5% {
+    opacity: 1;
+  }
+  10% {
+    opacity: 0;
+  }
+  20% {
+    opacity: 1;
+  }
+  40% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+  80% {
+    opacity: 1;
+  }
+  90% {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+const IndicatorOn = styled.div`
+  position: absolute;
+  left: ${props => props.index}px;
+  top: ${props => props.index * 31}px;
+  width: 48px;
+  height: 42px;
+  animation: ${onAnimation} 800ms ease-in;
+  background: center center no-repeat url('${imgOn}');
+`;
+
+const storeKeys = ['pieces', 'fioles', 'binary', 'simon', 'fuses', 'lights', 'wires'];
+
+const Indicators = function({ store }) {
+  return (
+    <IndicatorsContainer>
+      {storeKeys.map((k, i) => (
+        <React.Fragment>
+          <IndicatorOff key={`k${i}`} index={i} />
+          {store[k].SOLVED ? <IndicatorOn key={`k${i}`} index={i} /> : null}
+        </React.Fragment>
+      ))}
+    </IndicatorsContainer>
+  );
+};
+
+export default Indicators;
