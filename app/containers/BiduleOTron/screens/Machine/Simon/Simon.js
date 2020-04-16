ede7c739d1f9e8760fb4f2ba839023eb764d2486
Frédéric Bonjour <fred@habilebill.fr>
2020-04-16 10:53:54 +0200

    SVG for simon.

--- app/containers/BiduleOTron/screens/Machine/Simon/Simon.js
+++ app/containers/BiduleOTron/screens/Machine/Simon/Simon.js
@@ -6,48 +6,95 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
 
-import withFocus from 'BOT/withFocus';
+import Indicator from './img/simon.svg';
+import Antenna from './img/antenne.svg';
 
-function importImages() {
-  const r = require.context('./img/', false, /couleur_.*\.png$/);
-  const images = {};
-  r.keys().forEach(key => {
-    const l = key.match(/(\d)/)[1];
-    const on = key.match(/_on_/);
-    if (!images[l]) {
-      images[l] = [];
-    }
-    images[l][on ? 1 : 0] = r(key);
-  });
-  return images;
-}
-const images = importImages();
+import withFocus from 'BOT/withFocus';
+import theme from 'BOT/Theme';
 
 const MachineSimonContainer = styled.div`
   position: absolute;
-  top: 256px;
+  top: 257px;
   left: 749px;
   width: 119px;
   height: 253px;
-  display: flex;
-  flex-flow: column;
-  opacity: 0;
-  transition: opacity 250ms ease;
+  display: grid;
+  grid-template-columns: 52px;
+  grid-gap: 16.5px;
+  grid-auto-rows: 12px;
   padding: 80px 0 0 30px;
 
-  &.focused,
-  &.solved {
+  &.focused .indicator,
+  &.solved .indicator {
     opacity: 1;
+  }
+
+  .indicator {
+    opacity: 0;
+    transition: opacity 250ms ease;
+  }
+
+  .indicator-1.on path[class$="fill-color"] {
+    fill: ${theme.orangeLight};
+  }
+  .indicator-2.on path[class$="fill-color"] {
+    fill: ${theme.electricBlueLight};
+  }
+  .indicator-3.on path[class$="fill-color"] {
+    fill: ${theme.yellowLight};
+  }
+  .indicator-4.on path[class$="fill-color"] {
+    fill: ${theme.pinkLight};
+  }
+  .indicator-5.on path[class$="fill-color"] {
+    fill: ${theme.purpleLight};
+  }
+
+  .antenna {
+    position: absolute;
+    top: -48px;
+    left: 25px;
+
+    path.antenne_svg__step {
+      opacity: 0;
+      transition: opacity 250ms ease;
+    }
+
+    &.step-1 path.antenne_svg__step-1 {
+      opacity: 1;
+    }
+    &.step-2 path.antenne_svg__step-2 {
+      opacity: 1;
+    }
+    &.step-3 path.antenne_svg__step-3 {
+      opacity: 1;
+    }
+    &.step-4 path.antenne_svg__step-4 {
+      opacity: 1;
+    }
+    &.step-5 path.antenne_svg__step-5 {
+      opacity: 1;
+    }
+    &.step-6 path.antenne_svg__step-6 {
+      opacity: 1;
+    }
+    &.step-7 path.antenne_svg__step-7 {
+      opacity: 1;
+    }
+    &.step-8 path.antenne_svg__step-8 {
+      opacity: 1;
+    }
   }
 `;
 
+/*
 const Indicator = styled.div`
   width: 52px;
   height: 15px;
   margin-bottom: 14px;
   background: top left no-repeat url('${props => images[props.index][props.on ? 1 : 0]}');
 `;
-
+*/
 const MachineSimon = function({ dispatch, simon, focused, solved }) {
 
   let timer;
@@ -84,8 +131,9 @@ const MachineSimon = function({ dispatch, simon, focused, solved }) {
 
   return (
     <MachineSimonContainer className={classnames({ focused, solved })}>
+      <Antenna className={`antenna step-${simon.length - 1}`} />
       {[1, 2, 3, 4, 5].map(i => (
-        <Indicator key={i} index={i} on={solved || (playingSeq && simon.desired[simon.cursor] === i)} />
+        <Indicator key={i} className={classnames('indicator', `indicator-${i}`, { on: solved || (playingSeq && simon.desired[simon.cursor] === i) })} />
       ))}
     </MachineSimonContainer>
   );
