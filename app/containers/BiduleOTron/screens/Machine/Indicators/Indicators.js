ea55659ae2c8143bb6896e19e4e5061b705410c0
Frédéric Bonjour <fred@habilebill.fr>
2020-04-16 10:52:54 +0200

    SVG for Indicators.

--- app/containers/BiduleOTron/screens/Machine/Indicators/Indicators.js
+++ app/containers/BiduleOTron/screens/Machine/Indicators/Indicators.js
@@ -1,25 +1,10 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
 
-import imgOn from './img/ampoule_on.png';
-import imgOff from './img/ampoule_off.png';
+import IndicatorOn from './img/lampe_on.svg';
+import IndicatorOff from './img/lampe_off.svg';
 
-const IndicatorsContainer = styled.div`
-  position: absolute;
-  top: 90px;
-  left: 1120px;
-  width: 60px;
-  height: 260px;
-`;
-
-const IndicatorOff = styled.div`
-  position: absolute;
-  left: ${props => props.index}px;
-  top: ${props => props.index * 31}px;
-  width: 48px;
-  height: 42px;
-  background: center center no-repeat url('${imgOff}');
-`;
+const storeKeys = ['pieces', 'pipes', 'binary', 'simon', 'fuses', 'lights', 'wires'];
 
 const onAnimation = keyframes`
   from {
@@ -51,25 +36,33 @@ const onAnimation = keyframes`
   }
 `;
 
-const IndicatorOn = styled.div`
+const IndicatorsContainer = styled.div`
   position: absolute;
-  left: ${props => props.index}px;
-  top: ${props => props.index * 31}px;
-  width: 48px;
-  height: 42px;
-  animation: ${onAnimation} 800ms ease-in;
-  background: center center no-repeat url('${imgOn}');
-`;
+  top: 90px;
+  left: 1116px;
+  width: 60px;
+  height: 260px;
+  z-index: -1;
 
-const storeKeys = ['pieces', 'pipes', 'binary', 'simon', 'fuses', 'lights', 'wires'];
+  .i {
+    position:absolute;
+  }
+  .i.on {
+    animation: ${onAnimation} 800ms ease-in;
+  }
+  ${storeKeys.reduce((acc, v, i) => {
+    acc += `.i${i}{left:${i}px;top:${i * 31}px;}\n`;
+    return acc;
+  }, '')}
+`;
 
 const Indicators = function({ store }) {
   return (
     <IndicatorsContainer>
       {storeKeys.map((k, i) => (
         <React.Fragment key={`k${i}`}>
-          <IndicatorOff index={i} />
-          {store[k].SOLVED ? <IndicatorOn index={i} /> : null}
+          <IndicatorOff className={`i i${i}`} />
+          {store[k].SOLVED ? <IndicatorOn className={`i i${i} on`} /> : null}
         </React.Fragment>
       ))}
     </IndicatorsContainer>
