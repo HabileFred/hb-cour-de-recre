f529e309f332376094020f10d12ba9a022c56665
Frédéric Bonjour <fred@habilebill.fr>
2020-04-20 18:44:04 +0200

    Fix #5

--- app/containers/BiduleOTron/screens/Home/Home.js
+++ app/containers/BiduleOTron/screens/Home/Home.js
@@ -13,6 +13,7 @@ import theme from 'BOT/Theme';
 import Background from './img/menu.svg';
 import ImageMenuItem1 from './img/menu_selection_bidulo.svg';
 import ImageMenuItem2 from './img/menu_selection_lance.svg';
+import ImageSubmit from 'BOT/img/valider.svg';
 
 const animation = keyframes`
   from {
@@ -53,6 +54,23 @@ const Wrapper = styled.section`
     stroke: ${theme.focus.color};
     fill: ${theme.focus.color};
   }
+
+  .btn-submit {
+    position: absolute;
+    top: 357px;
+    opacity: 0;
+    transition: opacity 250ms ease-in;
+  }
+  .btn-submit.active {
+    opacity: 1;
+  }
+
+  .btn-submit-1 {
+    left: 167px;
+  }
+  .btn-submit-2 {
+    left: 452px;
+  }
 `;
 
 function Home({ home }) {
@@ -64,6 +82,12 @@ function Home({ home }) {
       </div>
       <div className={classnames('menu-item', 'menu-item-2', { active: home.cursor === 1 })}>
         <ImageMenuItem2 />
+      </div>
+      <div className={classnames('btn-submit', 'btn-submit-1', { active: home.cursor === 0 })}>
+        <ImageSubmit />
+      </div>
+      <div className={classnames('btn-submit', 'btn-submit-2', { active: home.cursor === 1 })}>
+        <ImageSubmit />
       </div>
     </Wrapper>
   );
