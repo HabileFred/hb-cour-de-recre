9c7ea75e7af3472b31b29df869e6077801e7b996
Frédéric Bonjour <fred@habilebill.fr>
2020-04-23 17:22:19 +0200

    Moving "Aucun signal" message on Off screen.

--- app/containers/BiduleOTron/screens/Off/Off.js
+++ app/containers/BiduleOTron/screens/Off/Off.js
@@ -1,11 +1,12 @@
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
 import styled from 'styled-components';
 
-import OffScreen from './img/ecran_eteint.svg';
+import OffScreen from './img/ordi_ecran_eteint.svg';
+import NoSignal from './img/ordi_ecran_eteint_texte_blanc.svg';
 
 const Wrapper = styled.section`
   top: 0;
@@ -15,12 +16,34 @@ const Wrapper = styled.section`
   display: flex;
   flex-flow: row;
   padding: 230px 466px;
+  position: absolute;
+
+  .no-signal {
+    transform: scale(.8);
+    transform-origin: center;
+    position: absolute;
+  }
 `;
 
-function Off({ dispatch }) {
+function Off() {
+
+  const getRandomX = () => 30 + Math.floor(Math.random() * 700);
+  const getRandomY = () => 30 + Math.floor(Math.random() * 390);
+
+  const [noSignal, setNoSignal] = useState({ x: getRandomX(), y: getRandomY() });
+
+  useEffect(() => {
+    let timer = window.setInterval(() => {
+      setNoSignal({ x: getRandomX(), y: getRandomY() });
+    }, 2000);
+
+    return () => window.clearInterval(timer);
+  }, []);
+
   return (
     <Wrapper>
       <OffScreen style={{ position: 'absolute', top: 0, left: 0 }}/>
+      <NoSignal className="no-signal" style={{ top: `${noSignal.y}px`, left: `${noSignal.x}px` }}/>
     </Wrapper>
   );
 }
